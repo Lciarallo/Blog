86783c0b965f124801e715bfa51e9bc58c05b4e4
Lciarallo <luizeduardociarallo@gmail.com>
2024-03-27 10:49:53 -0300

    Footer add no candidatar

--- src/app/candidatar/candidatar.component.ts
+++ src/app/candidatar/candidatar.component.ts
@@ -1,15 +1,14 @@
 
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
-  selector: 'app-candidatar',
-  templateUrl: './candidatar.component.html',
-  styleUrl: './candidatar.component.scss',
-  standalone: true,
-  imports: [FormsModule]
-    
-
+    selector: 'app-candidatar',
+    templateUrl: './candidatar.component.html',
+    styleUrl: './candidatar.component.scss',
+    standalone: true,
+    imports: [FormsModule, FooterComponent]
 })
 export class CandidatarComponent {
   nome = document.getElementById("inputnome")
